7a8b1a55279276a3a335a5ff0015c213bd82bd7e
RyanDur <ryan.p.durling@gmail.com>
2013-12-13 09:38:45 +0000

    remove duplicate test

--- src/test/java/TestSpamBot.java
+++ src/test/java/TestSpamBot.java
@@ -18,15 +18,17 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestSpamBot{
-	SpamBot sbr; 
-	String urlStr ="http://www.google.com"; //"something that isnt a URL" 
-	int numOfThreads = 8;
-	@Before
-	public void beforeTest(){
-		sbr = spy(new SpamBotImpl());
-		sbr.setSeed(urlStr);
-	}
-	 /**
+    SpamBot sbr;
+    String urlStr ="http://www.google.com"; //"something that isnt a URL"
+    int numOfThreads = 8;
+
+    @Before
+    public void beforeTest(){
+        sbr = spy(new SpamBotImpl());
+        sbr.setSeed(urlStr);
+    }
+
+    /**
      * Sets the seed.
      *
      * The seed is the very first URL that has to be given to the
@@ -36,22 +38,10 @@ public class TestSpamBot{
      *
      * @param seedUrl the first URL to fetch and analyse
      */
-	@Test
-    public void testSetSeed() {
-		try {
-				URL url = new URL(urlStr);
-				assertEquals(sbr.getSeed(), urlStr);
-		} catch (MalformedURLException e) {
-				assertEquals(sbr.getSeed(), "not a URL");
-		}
-	}
-    /**
-     * Returns the seed URL. * @return the seed URL.
-     */
-	@Test
-    public void testGetSeed(){
-		assertEquals(sbr.getSeed(), urlStr);
-	}
+    @Test
+    public void testSetSeed() throws MalformedURLException {
+        assertEquals(sbr.getSeed(), urlStr);
+    }
 
     /**
      * Sets the number of threads.
@@ -61,50 +51,35 @@ public class TestSpamBot{
      *
      * @param count the number of threads (i.e. crawlers) to start in parallel
      */
-
-	@Test
+    @Test
     public void testSetThreads(){
-		sbr.setThreads(numOfThreads);
-		assertEquals(sbr.getNumOfThreads(), numOfThreads);
-
-	}
+        sbr.setThreads(numOfThreads);
+        assertEquals(sbr.getNumOfThreads(), numOfThreads);
+    }
 
     /**
      * Initiates the scanning process.
      */
-	@Test
-    public void shouldBeAbleToScanASite() throws FileNotFoundException, IOException {	
-		String fileDir = "./emails.txt";
-		File emails = new File(fileDir);
-		assertTrue(emails.isFile());
-		
-		WebPage wp = mock(WebPage.class);
-		Set<String> testLinks = new HashSet<String>();
-		Set<String> testEmails = new HashSet<String>();
-		testEmails.add("foo");
-		testEmails.add("bar");
-		doReturn(wp).when(sbr).getWebPage();
-		when(wp.getLinks()).thenReturn(testLinks);
-		when(wp.getEmails()).thenReturn(testEmails);
-		
-		sbr.scanSite();
-		String[] emailArray = wp.getEmails().toArray(new String[testEmails.size()]);
-		
-	
-		assertEquals(emailArray, getFromFile(fileDir));
-	
-	}
-	
-	private String[] getFromFile(String fileDir) throws FileNotFoundException, IOException{
-		FileInputStream fin =  new FileInputStream(fileDir);
-		BufferedReader myInput = new BufferedReader(new InputStreamReader(fin));
-		ArrayList<String> sb = new ArrayList<String>();
-		String thisLine = null;
- 		while ((thisLine = myInput.readLine()) != null) {  
-			sb.add(thisLine);
-		}
-		return sb.toArray(new String[sb.size()]);
-	}
+    @Test
+    public void shouldBeAbleToScanASite() throws FileNotFoundException, IOException {
+        String fileDir = "./emails.txt";
+        File emails = new File(fileDir);
+        assertTrue(emails.isFile());
+
+        WebPage wp = mock(WebPage.class);
+        Set<String> testLinks = new HashSet<String>();
+        Set<String> testEmails = new HashSet<String>();
+        testEmails.add("foo");
+        testEmails.add("bar");
+        doReturn(wp).when(sbr).getWebPage();
+        when(wp.getLinks()).thenReturn(testLinks);
+        when(wp.getEmails()).thenReturn(testEmails);
+
+        sbr.scanSite();
+        String[] emailArray = wp.getEmails().toArray(new String[testEmails.size()]);
+
+        assertEquals(emailArray, getFromFile(fileDir));
+    }
 
     /**
      * Returns all the emails gathered.
@@ -113,12 +88,22 @@ public class TestSpamBot{
      * have stopped. If it is called before that point, its
      * behaviour is not defined.
      */
-
-	@Test
+    @Test
     public void testGetEMails(){
-		//Set<String>
-		//assertEquals(get and populate array with each line on .rtf);
-//		assertEquals(1,2);
-	}
-//@cleanup destroy the file
+        //Set<String>
+        //assertEquals(get and populate array with each line on .rtf);
+        //              assertEquals(1,2);
+    }
+    //@cleanup destroy the file
+
+    private String[] getFromFile(String fileDir) throws FileNotFoundException, IOException{
+        FileInputStream fin =  new FileInputStream(fileDir);
+        BufferedReader myInput = new BufferedReader(new InputStreamReader(fin));
+        ArrayList<String> sb = new ArrayList<String>();
+        String thisLine = null;
+        while ((thisLine = myInput.readLine()) != null) {
+            sb.add(thisLine);
+        }
+        return sb.toArray(new String[sb.size()]);
+    }
 }
